f31597ff106595104f2ea674283144828652cb87
Yann Gilquin <ygilquin@gopro.com>
2017-05-17 11:07:52 +0200

    Multiresolution cube geometry support
    Add Pyramid background renderer organizing draw of the background in cube multiresolution
    Add FORGE.Tile : THREE.Mesh based object positionned in cube space to handle a texture tile
    
    Camera pointer controller panning and zooming is camera fov adaptative from now on
    
    Restrictions: at the moment pyramid loads new levels entirely and synchronously. Should be changed in next releases

--- src/controllers/ControllerPointer.js
+++ src/controllers/ControllerPointer.js
@@ -323,6 +323,8 @@ FORGE.ControllerPointer.prototype._wheelHandler = function(event)
     var delta = invert / this._zoom.hardness;
     var factorDeltaY = 1;
 
+    var hardness = (this._camera.fov / 90);
+
     if (event.data.deltaMode)
     {
         switch(event.data.deltaMode)
@@ -342,6 +344,8 @@ FORGE.ControllerPointer.prototype._wheelHandler = function(event)
     {
         delta *= (event.data.deltaY * factorDeltaY) / 5;
     }
+
+    delta *= hardness;
 
     this._camera.fov = this._camera.fov - delta;
     this.log("_wheelHandler (fov:" + this._camera.fov + ")");
@@ -408,6 +412,8 @@ FORGE.ControllerPointer.prototype.update = function()
 {
     var size = this._viewer.renderer.displayResolution;
     var hardness = 1 / (this._orientation.hardness * Math.min(size.width, size.height));
+
+    hardness *= (this._camera.fov / 90);
 
     this._velocity.subVectors(this._positionCurrent, this._positionStart);
 
